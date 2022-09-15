03738c1191130d575c22fa741806e230cce01581
Renato Adriano <renatoadriano.eprosec@gmail.com>
2022-09-15 16:03:02 +0000

    Django Login and Register System

--- Projeto/crm1/accounts/views.py
+++ Projeto/crm1/accounts/views.py
@@ -1,13 +1,66 @@
 from django.shortcuts import render, redirect 
 from django.http import HttpResponse
 from django.forms import inlineformset_factory
+from django.contrib.auth.forms import UserCreationForm
+
+from django.contrib.auth import authenticate, login, logout
+
+from django.contrib import messages
+
+from django.contrib.auth.decorators import login_required
+
+from accounts.decorators import unauthenticated_user, allowed_users
+
 # Create your views here.
 from .models import *
-from .forms import OrderForm
+from .forms import OrderForm, CreateUserForm
 from .filters import OrderFilter
 
+@unauthenticated_user
+def registerPage(request):
+    
+	form = CreateUserForm()
+	if request.method == 'POST':
+		form = CreateUserForm(request.POST)
+		if form.is_valid():
+			form.save()
+			user = form.cleaned_data.get('username')
+			messages.success(request, 'Account was created for ' + user)
+
+			return redirect('login')
+			
+
+	context = {'form':form}
+	return render(request, 'accounts/register.html', context)
+
+@unauthenticated_user
+def loginPage(request):
+    
+	if request.user.is_authenticated:
+		return redirect('home')
+	else:
+		if request.method == 'POST':
+			username = request.POST.get('username')
+			password =request.POST.get('password')
+
+			user = authenticate(request, username=username, password=password)
+
+			if user is not None:
+				login(request, user)
+				return redirect('home')
+			else:
+				messages.info(request, 'Username OR password is incorrect')
+
+		context = {}
+		return render(request, 'accounts/login.html', context)
+
+def logoutUser(request):
+	logout(request)
+	return redirect('login')
 
 
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['admin'])
 def home(request):
 	orders = Order.objects.all()
 	customers = Customer.objects.all()
@@ -24,11 +77,13 @@ def home(request):
 
 	return render(request, 'accounts/dashboard.html', context)
 
+@login_required(login_url='login')
 def products(request):
 	products = Product.objects.all()
 
 	return render(request, 'accounts/products.html', {'products':products})
 
+@login_required(login_url='login')
 def customer(request, pk_test):
 	customer = Customer.objects.get(id=pk_test)
 
@@ -36,12 +91,13 @@ def customer(request, pk_test):
 	order_count = orders.count()
 
 	myFilter = OrderFilter(request.GET, queryset=orders)
-	orders = myFilter.qs
+	orders = myFilter.qs 
 
-	context = {'customer':customer, 'orders':orders, 'order_count':order_count, 'myFilter': myFilter}
+	context = {'customer':customer, 'orders':orders, 'order_count':order_count,
+	'myFilter':myFilter}
 	return render(request, 'accounts/customer.html',context)
 
-
+@login_required(login_url='login')
 def createOrder(request, pk):
 	OrderFormSet = inlineformset_factory(Customer, Order, fields=('product', 'status'), extra=10 )
 	customer = Customer.objects.get(id=pk)
@@ -49,7 +105,7 @@ def createOrder(request, pk):
 	#form = OrderForm(initial={'customer':customer})
 	if request.method == 'POST':
 		#print('Printing POST:', request.POST)
-		#form = OrderForm(request.POST)
+		form = OrderForm(request.POST)
 		formset = OrderFormSet(request.POST, instance=customer)
 		if formset.is_valid():
 			formset.save()
@@ -58,6 +114,7 @@ def createOrder(request, pk):
 	context = {'form':formset}
 	return render(request, 'accounts/order_form.html', context)
 
+@login_required(login_url='login')
 def updateOrder(request, pk):
 
 	order = Order.objects.get(id=pk)
@@ -72,6 +129,7 @@ def updateOrder(request, pk):
 	context = {'form':form}
 	return render(request, 'accounts/order_form.html', context)
 
+@login_required(login_url='login')
 def deleteOrder(request, pk):
 	order = Order.objects.get(id=pk)
 	if request.method == "POST":
@@ -79,4 +137,4 @@ def deleteOrder(request, pk):
 		return redirect('/')
 
 	context = {'item':order}
-	return render(request, 'accounts/delete.html', context)	
+	return render(request, 'accounts/delete.html', context)
